eb8ac451a95177ae7693b4ae52e19fa011ca2318
redhot075 <code@redhot075.com>
2026-02-24 17:30:38 +0300

    feat: add BaseToken ERC20 contract with mint

--- contracts/BaseToken.sol
+++ contracts/BaseToken.sol
@@ -1,9 +1,1 @@
-```
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract BaseToken is ERC20 {
-    constructor() ERC20("BaseToken", "BTK") {}
-}
+pragma solidity ^0.8.20;\n\nimport "@openzeppelin/contracts/token/ERC20/ERC20.sol";\n\ncontract BaseToken is ERC20 {\n  constructor() ERC20("BaseToken", "BTK") {\n    _mint(msg.sender, 1000000 * 10 ** decimals());\n  }\n}
